b8e8664f066f7e7680d62d06efc0f64bcce6a119
Furkat Gofurov <furkat.gofurov@suse.com>
2024-11-18 15:08:15 +0200

    Add skeleton code for webhooks
    Signed-off-by: Furkat Gofurov <furkat.gofurov@suse.com>

--- cmd/main.go
+++ cmd/main.go
@@ -149,6 +149,12 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "KubewardenAddon")
 		os.Exit(1)
 	}
+
+	if err = (&addonv1alpha1.KubewardenAddon{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "KubewardenAddon")
+		os.Exit(1)
+	}
+
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
